d491e43e020585ec9d0e1af5aef28adecd0d256d
WildanHF <154210689+WildanHF@users.noreply.github.com>
2024-03-18 03:39:56 +0700

    merge done

--- MOD5/Program.cs
+++ MOD5/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace MOD5
 {
+   class DataGeneric<T> {
+        T Data { get; set; }
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        }
+    }
     public class HaloGeneric
     {
         public void SapaUser<T>(T userName)
@@ -16,11 +27,18 @@ namespace MOD5
 
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            DataGeneric<string> stringData = new DataGeneric<string>("Y");
+            stringData.PrintData(); 
+            DataGeneric<string> nimData = new DataGeneric<string>("1302223012");
+            nimData.PrintData(); 
+           
             HaloGeneric halo = new HaloGeneric();
 
-
+            
             string namaPanggilan = "Fernando";
             halo.SapaUser(namaPanggilan);
             Console.Read();
